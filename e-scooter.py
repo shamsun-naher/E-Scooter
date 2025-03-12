d0a8da71005c35e2beb905af00236f96b2817efa
shamsun-naher <s.naher.epst@gmail.com>
2025-03-12 14:07:08 +0100

    e-scooter.py

--- e-scooter.py
+++ e-scooter.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from script1 import function_from_script1
-from script2 import function_from_script2
+from temp import function_from_script1
+from future_forecasting import function_from_script2
 
 def main():
     st.title('Streamlit App Running Multiple Scripts')
@@ -8,13 +8,13 @@ def main():
     # Option to choose which script to run
     script_choice = st.radio('Select the script to run:', ['Script 1', 'Script 2'])
 
-    if script_choice == 'Script 1':
-        st.subheader('Running Script 1')
+    if script_choice == 'temp':
+        st.subheader('Running temp')
         result1 = function_from_script1()
         st.write(result1)
 
-    elif script_choice == 'Script 2':
-        st.subheader('Running Script 2')
+    elif script_choice == 'future_forecasting':
+        st.subheader('Running future_forecasting')
         result2 = function_from_script2()
         st.write(result2)
 
